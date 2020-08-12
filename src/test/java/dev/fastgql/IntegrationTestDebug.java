e38fdb71b7e37cee866e391e0114c644568caaff
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2020-08-12 15:13:53 +0200

    passing test

--- src/test/java/dev/fastgql/IntegrationTestDebug.java
+++ src/test/java/dev/fastgql/IntegrationTestDebug.java
@@ -3,20 +3,19 @@ package dev.fastgql;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import dev.fastgql.db.DatasourceConfig;
+import dev.fastgql.db.DebeziumConfig;
+import dev.fastgql.events.DebeziumEngineSingleton;
 import dev.fastgql.integration.*;
 import dev.fastgql.modules.DatabaseModule;
 import dev.fastgql.modules.VertxModule;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.http.HttpClientOptions;
+import io.debezium.engine.ChangeEvent;
+import io.reactivex.Flowable;
 import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.reactivex.core.Vertx;
-import io.vertx.reactivex.core.http.HttpClient;
-import io.vertx.reactivex.ext.web.client.WebClient;
 import io.vertx.reactivex.sqlclient.Pool;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,6 +23,7 @@ import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.lifecycle.Startables;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
@@ -37,15 +37,16 @@ public class IntegrationTestDebug {
     .withUsername("debezium")
     .withPassword("dbz");
 
-  private String deploymentID;
   private JsonObject config;
   private JsonObject configMultipleQueries;
+  private DatasourceConfig datasourceConfig;
+  private DebeziumConfig debeziumConfig;
 
   @BeforeEach
   public void beforeEach(Vertx vertx, VertxTestContext context) {
     Startables.deepStart(Stream.of(jdbcDatabaseContainer)).join();
 
-    DatasourceConfig datasourceConfig =
+    datasourceConfig =
       DatasourceConfig.createDatasourceConfig(
         jdbcDatabaseContainer.getJdbcUrl(),
         jdbcDatabaseContainer.getUsername(),
@@ -76,29 +77,14 @@ public class IntegrationTestDebug {
             "password", datasourceConfig.getPassword(),
             "schema", datasourceConfig.getSchema()));
 
-    DeploymentOptions options = new DeploymentOptions().setConfig(config);
-    vertx
-      .rxDeployVerticle(new FastGQL(), options)
-      .subscribe(
-        deploymentID -> {
-          this.deploymentID = deploymentID;
-          context.completeNow();
-        });
-  }
+    debeziumConfig = DebeziumConfig.createWithJsonConfig(config.getJsonObject("debezium"));
 
-  @AfterEach
-  public void afterEach(Vertx vertx, VertxTestContext context) {
-    vertx
-      .rxUndeploy(deploymentID)
-      .subscribe(
-        () -> {
-          jdbcDatabaseContainer.close();
-          context.completeNow();
-        });
+    context.completeNow();
   }
 
   @Test
-  public void shouldReceiveResponse(Vertx vertx, VertxTestContext context) {
+  @Timeout(value = 10, timeUnit = TimeUnit.DAYS)
+  public void shouldReceiveResponse(Vertx vertx, VertxTestContext context) throws IOException {
     String directory = "subscriptions/simple/referencing";
 
     Injector injector =
@@ -109,35 +95,31 @@ public class IntegrationTestDebug {
       Guice.createInjector(new VertxModule(vertx, configMultipleQueries), new DatabaseModule());
     Pool poolMultipleQueries = injectorMultipleQueries.getInstance(Pool.class);
 
-    System.out.println(String.format("Test: %s", directory));
+    DebeziumEngineSingleton debeziumEngineSingleton = new DebeziumEngineSingleton(datasourceConfig, debeziumConfig);
+    debeziumEngineSingleton.startNewEngine();
 
-    WebClient client = WebClient.create(vertx);
-    HttpClient httpClient =
-      vertx.createHttpClient(new HttpClientOptions().setDefaultPort(8081));
-    String query = String.format("%s/query.graphql", directory);
-    List<String> expected =
-      List.of(
-        String.format("%s/expected-1.json", directory),
-        String.format("%s/expected-2.json", directory));
+    Flowable<ChangeEvent<String, String>> eventFlowable = debeziumEngineSingleton.getChangeEventFlowable()
+      .filter(event -> List.of("dbserver.test.addresses", "dbserver.test.customers").contains(event.destination()));
+
+    eventFlowable
+      .delay(2, TimeUnit.SECONDS)
+      .parallel().flatMap(event -> pool.rxBegin().toFlowable())
+      .flatMap(transaction -> transaction.rxQuery("LOCK TABLES customers as a0 READ, addresses as a1 READ").doOnSuccess(rows -> System.out.println("LOCKED")).toFlowable()
+        .flatMap(rows -> transaction.rxQuery("SELECT a0.address AS a0_address, a1.id AS a1_id, a1.street AS a1_street, a0.id AS a0_id FROM customers a0 LEFT JOIN addresses a1 ON a0.address = a1.id ORDER BY a0.id ASC").doOnSuccess(rows2 -> System.out.println("QUERY")).toFlowable())
+        .flatMap(rows -> transaction.rxQuery("UNLOCK TABLES").doOnSuccess(rows2 -> System.out.println("UNLOCKED")).toFlowable())
+        .flatMap(rows -> transaction.rxCommit().toFlowable())
+      )
+      .sequential()
+      .subscribe();
 
     DBTestUtils.executeSQLQuery(Paths.get(directory, "init.sql").toString(), poolMultipleQueries)
-      .flatMap(
-        rows ->
-          client
-            .get(8081, "localhost", "/update")
-            .rxSend()
-            .flatMap(response -> httpClient.rxWebSocket("/graphql"))
-            .flatMapCompletable(
-              webSocket ->
-                GraphQLTestUtils.startSubscription(query, context, webSocket)
-                  .andThen(
-                    GraphQLTestUtils.verifySubscription(
-                      expected, context, webSocket)))
-            .delay(10, TimeUnit.SECONDS)
-            .andThen(
-              DBTestUtils.executeSQLQuery(
-                String.format("%s/query.sql", directory), pool)))
+      .delay(10, TimeUnit.SECONDS)
+      .flatMap(rows -> DBTestUtils.executeSQLQuery(String.format("%s/query.sql", directory), pool))
+      .doFinally(debeziumEngineSingleton::stopEngine)
       .subscribe(
-        rows -> System.out.println("affected rows: " + rows.rowCount()), context::failNow);
+        rows -> {
+          System.out.println("affected rows: " + rows.rowCount());
+          context.completeNow();
+        }, context::failNow);
   }
 }
